187de51f9deb6fc87733e1fef822ba310d5be516
Cassie Kise <cassandrakise@gmail.com>
2022-11-13 23:49:05 -0800

    added apollo provider in app.js

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,6 +1,5 @@
 import { gql } from '@apollo/client';
 
-
 export const LOGIN_USER = gql`
     mutation loginUser($email: String!, $password: String!) {
         loginUser(email: $email, password: $password) {
@@ -42,27 +41,3 @@ export const REMOVE_BOOK = gql`
         }
     }
 `;
-
-// below is example from mini project
-
-// export const CREATE_MATCHUP = gql`
-//   mutation createMatchup($tech1: String!, $tech2: String!) {
-//     createMatchup(tech1: $tech1, tech2: $tech2) {
-//       _id
-//       tech1
-//       tech2
-//     }
-//   }
-// `;
-
-// export const CREATE_VOTE = gql`
-//   mutation createVote($_id: String!, $techNum: Int!) {
-//     createVote(_id: $_id, techNum: $techNum) {
-//       _id
-//       tech1
-//       tech2
-//       tech1_votes
-//       tech2_votes
-//     }
-//   }
-// `;
